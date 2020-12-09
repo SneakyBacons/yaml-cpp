48d58b758805926d165d3fdcb96ec91e2a87eb86
Scott Balfour <scott2004b@hotmail.com>
2020-12-09 07:09:37 +0000

    [~] Adding premake

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+
+    targetdir("bin/%{prj.name}/" .. outputdir )
+    objdir("bin-int/%{prj.name}/" .. outputdir )
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+        cppdialect "C++17"
+        
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Development"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
